3e0b033651b990682ebc0872115bd67ae2ed671a
Senama <sergeenama@gmail.com>
2018-10-07 15:18:46 -0400

    Problem num 2

--- pset1.js
+++ pset1.js
@@ -17,3 +17,30 @@ function myAge( ageNow, numYears ) {
 }
  let newAge = myAge (28,32);
  console.log(newAge);
+
+ /******************
+Area of A Circle
+
+Write a function called areaOfaCircle()
+
+The function will calculate the area of circle
+It will take one argument which will be the radius
+This is the formula to calculate the area of a circle:
+A = π * r2, where is π is Pi and r is the radius squared
+******************/
+
+
+function areaOfaCircle(radius){
+    console.log('radius is...', radius)
+    
+    return area;
+       
+   }    
+   
+   console.log(areaOfCircle(1))
+   /*
+       radius is... 1
+       0
+   
+   /*
+       radius is... undefined
