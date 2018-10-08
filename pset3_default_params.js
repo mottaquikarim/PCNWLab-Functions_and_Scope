08af16af138c4106d03e33421e2a34dd31dcc3f5
Robert Abreu <robertabreu@pursuit.org>
2018-10-07 22:54:44 -0400

    solution: problem 1 pset 3

--- pset3_default_params.js
+++ pset3_default_params.js
@@ -10,7 +10,12 @@
     
     use a default parameter for the middleName, set it to ""
 */
+function getFullName(firstName, middleName, lastName){
 
+    return `${firstName} ${middleName} ${lastName}`;
+}
+
+console.log(getFullName("Robert", "", "Abreu"));
 
 /*
     PROBLEM 2:
