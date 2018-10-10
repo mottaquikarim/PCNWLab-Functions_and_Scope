ec0cc01f6e66cd0b9a8f7fae4c5471fa829de40f
Kevin Zheng <kevin.zheng4783@gmail.com>
2018-10-09 20:58:09 -0400

    Finished pset2

--- pset2.js
+++ pset2.js
@@ -9,8 +9,15 @@
                  returns that computed value
     @example doMath( 1,2,3 ); // 9
 */
-
-
+function doMath(num1, num2, num3){
+    let ans = (num1 + num2) * num3;
+    return ans;
+}
+console.log(`Num 1: 9
+Num2: 6
+Num3: 3
+Answer: ` + doMath(9,6,3)) //Domath 9 + 6 = 15. 15 * 3 = 45
+console.log();//New line for organization.
 /*
     @function addMiddleName
     @param firstName
@@ -24,8 +31,25 @@
                  return firstname and last name
     @example addMiddleNmae('John', 'Mabel', 'Smith'); // John Mabel Smith
 */
-
-
+function addMiddleName(f_name, m_name, l_name){    
+    if(l_name == ""){
+        l_name = m_name;
+        let newName = f_name + " " + l_name;
+        return newName;
+    }
+    else if(m_name === ""){
+        let newName = f_name + " " + l_name;
+        return newName;
+    }
+    else{
+        let newName = f_name + " " + m_name + " " + l_name;
+        return newName;
+    }
+}
+console.log("My name is " + addMiddleName("John", "Missing", "Doe"));//Full name with middle name.
+console.log("My name is " + addMiddleName("Tommy","","Pom"));//Full name with correct blank middle name format.
+console.log("My name is " + addMiddleName("Richard","Marrow",""));//Full name but entered last name as middle name.
+console.log();//New line for organization.
 /*
     @function defaultPlaceholder
     @param placeholder {string}
@@ -35,8 +59,12 @@
     @example defaultPlaceHolder('Hello Wrold!'); 
              // <input type="text" placeholder="Hello Wrold!" />
 */
-
-
+function defaultPlaceHolder(holder="default"){
+    return holder;
+}
+console.log("The new placeholder is '" + defaultPlaceHolder() + "'");
+console.log("The new placeholder is '" + defaultPlaceHolder("Helloworld.") + "'");
+console.log();//New line for organization.
 /*
     @function addClasses
     @param {string} class1
@@ -47,8 +75,11 @@
     @example addClass('foo', 'bar', 'stuff');
              // <div class="foo bar">stuff</div>
 */
+function addClasses(class1, class2, content){
 
+}
 
+console.log();//New line for organization.
 /*
     @function duplicate
     @param {string} start
@@ -57,5 +88,10 @@
                  returns duplicated string
     @example duplicate('hello!'); // hello!hello!
 */
+function duplicate(dupThis){
+    let multi = dupThis + dupThis;
+    return multi;
+}
 
+console.log("Duplicating 'apple' results in: " + duplicate("apple"));
 
