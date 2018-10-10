e187710a1ce281b449941788253b080927477bfb
Jennifer Perez <jenniferperez@pursuit.org>
2018-10-09 21:28:19 -0400

    pset5 prob

--- pset5_declarations_vs_expressions.js
+++ pset5_declarations_vs_expressions.js
@@ -14,6 +14,24 @@
         return the remainder of the sum of a,b,c divided by 3
 */ 
 
+/*function expression
+const blah = function (a=1,b=2,c=3){
+    return (a+b+c) % 3;
+}
+console.log(blah());
+*/
+
+/*function declaration
+const blah = function (a=1,b=2,c=3){
+    return (a+b+c) % 3;
+}
+console.log(blah()); */
+
+/*function fat arrow
+const blah = (a,b,c) =>{
+    return (a+b+c) % 3;
+}
+console.log(blah(1,2,3));*/
 
 /*
     PROBLEM 2:
@@ -23,6 +41,27 @@
         so, if a = 'one', b = 'two', c='three'
         then function will return 'one-two-three'
 */
+/*expression
+const blah = function (a,b,c)
+{
+    return (a+ "-"+ b+ "-" + c);
+}
+console.log(blah('hey','you','wtv'));*/
+
+
+/* fat
+const blah =  (a,b,c) => 
+{
+    return (a+ "-"+ b+ "-" + c);
+}
+console.log(blah('hey','you','wtv'))*/
+
+/* function declaration
+function blah(a=1,b=2,c=3)
+{
+    return (a+ "-"+ b+ "-" + c);
+}
+console.log(blah('hey','you','wtv'));8/
 
 /*
     PROBLEM 3:
@@ -30,3 +69,25 @@
         return a random number between 0 and 10 
         (doesn't have to be a whole number tho)
 */
+
+/*
+const blah = function (a,b,c)
+{
+    return (a+ "-"+ b+ "-" + c);
+}
+console.log(blah());
+*/
+
+/*fat
+const blah =  (a,b,c) => 
+{
+    return (a+ "-"+ b+ "-" + c);
+}
+console.log(blah())*/
+
+/* function declaration
+function blah()
+{
+    return (a+ "-"+ b+ "-" + c);
+}
+console.log(blah());*/
