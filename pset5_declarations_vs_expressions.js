b0f82aaf57f1e0fb6ce0d87d4ad788dfa43d19b2
Syed Aziz <syedaziz27x@gmail.com>
2018-10-09 21:27:23 -0400

    finished questions

--- pset5_declarations_vs_expressions.js
+++ pset5_declarations_vs_expressions.js
@@ -15,19 +15,22 @@
 */ 
 
 //FAT ARROW 
-const remainderOfSum = (a,b,c) => {
-    return ((a + b + c)/3);
+const remainderOfSum1 = (a,b,c) => {
+    return ((a + b + c)%3);
 }
+console.log(remainderOfSum1(1,2,3));
 
 //FUNCTOIN DECLARATION
-function remainderOfSum(a,b,c){
-    return ((a + b + c)/3);
+function remainderOfSum2(a,b,c){
+    return ((a + b + c)%3);
 }
+console.log(remainderOfSum2(14,5,6));
 
 //FUNCTION EXPRESSION
-const remainderOfSum = functino(a,b,c){
-    return ((a + b + c)/3);
+const remainderOfSum3 = function(a,b,c) {
+    return ((a + b + c)%3);
 }
+console.log(remainderOfSum3(7,28,9));
 
 /*
     PROBLEM 2:
@@ -38,9 +41,49 @@ const remainderOfSum = functino(a,b,c){
         then function will return 'one-two-three'
 */
 
+//FAT ARROW
+const dashInBtwn1 = (a,b,c) => {
+    return `${a}-${b}-${c}` 
+}
+console.log(dashInBtwn1("a","b","c"));
+
+//FUNCTION DECLARATION
+function dashInBtwn2(a,b,c) {
+    return `${a}-${b}-${c}` 
+} 
+console.log(dashInBtwn2("a","b","c"));
+
+//FUNCTION EXPRESSION
+const dashInBtwn3 = function(a,b,c){
+    return `${a}-${b}-${c}` 
+}
+console.log(dashInBtwn3("a","b","c"));
+
 /*
     PROBLEM 3:
         Write a function that takes NO params
         return a random number between 0 and 10 
         (doesn't have to be a whole number tho)
+
 */
+
+//FAT ARROW
+
+const randomNum1 = () => {
+    return Math.random() * 10;
+}
+console.log(randomNum1());
+
+//FUNCTION DECALARATION
+
+function randomNum2() {
+    return Math.random() * 10;
+}
+console.log(randomNum2());
+
+//FUNCTION EXPRESSION
+
+const randomNum3 = function() {
+    return Math.random() * 10;
+}
+console.log(randomNum3());
