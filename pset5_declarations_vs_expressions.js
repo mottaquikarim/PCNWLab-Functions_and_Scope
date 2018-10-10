3a196a747104b439f9cc19fc70e3a4ee396c789c
Erika Perez <erikaperezvalle@pursuit.org>
2018-10-09 21:11:56 -0400

    completes pset5_declarations_vs_expressions.js

--- pset5_declarations_vs_expressions.js
+++ pset5_declarations_vs_expressions.js
@@ -14,6 +14,26 @@
         return the remainder of the sum of a,b,c divided by 3
 */ 
 
+function remainderOfSum1(a,b,c){
+    return (a + b +c) % 3;
+}
+console.log(remainderOfSum1(1,3,6));
+
+//----------------
+
+const remainderOfSum2 = function (a,b,c){
+    return (a + b +c) % 3;
+}
+
+console.log(remainderOfSum2(1,3,6));
+
+//--------------------
+
+const remainderOfSum3 = (a,b,c) =>{
+    return (a + b +c) % 3;
+}
+console.log(remainderOfSum3(1,3,6));
+
 
 /*
     PROBLEM 2:
@@ -24,9 +44,48 @@
         then function will return 'one-two-three'
 */
 
+function addDashes1(a,b,c){
+    return a+'_'+b+'_'+c;
+}
+console.log(addDashes1('one','two','three'));
+
+//-----------------------
+
+const addDashes2 = function (a,b,c){
+    return a+'_'+b+'_'+c;
+}
+console.log(addDashes2('one','two','three'));
+
+//----------------------
+
+const addDashes3 = (a,b,c)=>{
+    return a+'_'+b+'_'+c;
+}
+console.log(addDashes3('one','two','three'));
+
 /*
     PROBLEM 3:
         Write a function that takes NO params
         return a random number between 0 and 10 
         (doesn't have to be a whole number tho)
 */
+
+
+function randomNum1 (){
+    return Math.random ()*10;
+}
+console.log(randomNum1());
+
+//--------------
+
+const randomNum2 = function(){
+    return Math.random ()*10;
+}
+console.log(randomNum2());
+
+//-------------
+
+const randomNum3 = ()=>{
+    return Math.random ()*10;
+}
+console.log(randomNum3());
