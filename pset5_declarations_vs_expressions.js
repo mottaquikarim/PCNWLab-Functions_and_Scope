70d88654ec1d61c6e48beec44da6ffdb58a4f675
Jorgebillini <Jorgebillini@pursuit.org>
2018-10-09 21:41:59 -0400

    Added comments to differenciate between declaration, expression and fat arrow

--- pset5_declarations_vs_expressions.js
+++ pset5_declarations_vs_expressions.js
@@ -13,19 +13,20 @@
         Write a function that takes THREE params: a, b, c
         return the remainder of the sum of a,b,c divided by 3
 */ 
+// Function declaration 
 function remainderOf(a,b,c) { 
 let sum = a+b+c;
 return sum % 3;
 }
 console.log(remainderOf(6,5,6));
-
+// Function expression
 const myRemainderOf = function(a,b,c) {
     let sum = a+b+c;
     return sum % 3;
 
 }
 console.log(myRemainderOf(6,5,6));
-
+// Fat arrow declaration
 const theRemainderof = (a,b,c) => { 
     let sum = a+b+c;
     return sum % 3;
@@ -39,18 +40,19 @@ console.log(theRemainderof(6,5,6));
         so, if a = 'one', b = 'two', c='three'
         then function will return 'one-two-three'
 */
+// Function declaration
 function theNewString(a, b, c) { 
     let newstring = a+'-'+b+'-'+c;
     return newstring;
 }
 console.log(theNewString('1','2','3'));
-
+// Function expression
 const myNewString = function(a, b, c) {
     let newstring = a+'-'+b+'-'+c;
     return newstring;
 }
 console.log(myNewString('1','2','3'));
-
+// Fat Arrow expression 
 const newStringDawg = (a, b, c) => {
     let newstring = a+'-'+b+'-'+c;
     return newstring;
@@ -63,20 +65,22 @@ console.log(newStringDawg('1','2','3'));
         return a random number between 0 and 10 
         (doesn't have to be a whole number tho)
 */
+// Function declaration 
 function generateNumber() {
     let randomnum = Math.floor(Math.random()* 10);
     return randomnum;
 }
 console.log(generateNumber());
-
+// Function expression
 const makeNumber = function() {
     let randomnum = Math.floor(Math.random()* 10);
     return randomnum;
 }
 console.log(makeNumber());
-
+// Fat arrow expression
 const myPhatNumber = () => {
     let randomnum = Math.floor(Math.random()* 10);
     return randomnum;
 }
 console.log(myPhatNumber());
+
