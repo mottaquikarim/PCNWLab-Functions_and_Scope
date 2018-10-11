5b17d87d171954d5fcf9a499a25b15e1a2cd8a37
Robert Abreu <robertabreu@pursuit.org>
2018-10-10 21:09:04 -0400

    solution: problem 4 pset1

--- pset1.js
+++ pset1.js
@@ -106,10 +106,17 @@ A = π * r2, where is π is Pi and r is the radius squared
 ******************/
 
 
-function areaOfaCircle(radius){
-    
-    
-}    
+//function areaOfaCircle(radius){}; 
+
+const areaOfaCircle = function (radius){
+    return radius = Math.PI * (radius ** 2);
+};
+
+console.log(areaOfaCircle(5));
+
+const areaOfaCircle$ = (radius) => radius = Math.PI * (radius ** 2);
+
+console.log(areaOfaCircle$(5));
 
 /******************
 Temperature Converter Fahrenheit to Celsius
