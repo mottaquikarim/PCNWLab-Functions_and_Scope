c8b272369f5a8c98bf8fd39234a9e43309dbe2fc
Mielyn Acosta <mielynacosta@pursuit.org>
2018-10-31 13:09:41 -0400

    solved is it the weekend problem

--- pset1.js
+++ pset1.js
@@ -8,16 +8,20 @@ should return a number that tells you how old you'll be in numYears
 if that number is > 100, should return NaN
 if invalid input given, return -1
 ******************/
-
+// function declaration 
 function myAge( ageNow, numYears ) {
-    console.log("age Now value: " + ageNow);
-    console.log("num years value:" + numYears);
-
-    const ageLater = ageNow + numYears;
+    
+ const ageLater = ageNow + numYears;
     return ageLater; 
 }
 const MyAge1 = myAge(30, 2018)
 console.log(MyAge1);
+
+// function expression 
+
+const myAge2 = (ageNow2, numYears2) =>  numYears2 + ageNow2; 
+console.log(myAge2(30,2018)); 
+
 
 /******************
 Concatenate Strings
@@ -32,7 +36,7 @@ Concatenating string variables
     * console out mySentence variable
 - Call myConcatenate function
 ******************/
-
+//function declaration 
 function myConcatenate( firstStr, secondStr, thirdStr ) {
     
     // let mySentence = firstStr + ' ' + secondStr + ' ' + thirdStr; 
@@ -42,6 +46,10 @@ function myConcatenate( firstStr, secondStr, thirdStr ) {
 }
 console.log(myConcatenate('I', 'am', 'Mielyn ')); // 'I am iron man'
 
+// function expression 
+
+const myConcatenate2 = (firstStr1, secondStr1, thirdStr1) => firstStr1 + " " + secondStr1 + " " + thirdStr1;
+console.log(myConcatenate2('I', 'am', 'ironman'));
 
 /******************
 Subtract Function
@@ -51,13 +59,19 @@ Write a subtract function
 This function will take two numbers and subtract them
 Ensure that both of the inputs are numbers
 ******************/
-
+// function declaration 
 function subtract(a,b) {
     
     return a - b; 
     
 }
 console.log(subtract(5,3));
+
+//function expression 
+
+const subtract2 = (a,b) => a - b; 
+console.log(subtract(15,10));
+
 /******************
 Area of A Circle
 
@@ -69,14 +83,18 @@ This is the formula to calculate the area of a circle:
 A = π * r2, where is π is Pi and r is the radius squared
 ******************/
 
-
+// function declaration 
 function areaOfaCircle(radius){
     
-    const area = 2 ** Math.PI * radius; 
-    return area;
+    return 2 ** Math.PI * radius; 
+    
 
 }    
 console.log(areaOfaCircle(1));
+// function expression 
+
+const areaOfaCircle2 = radius => 2 ** Math.PI * radius;
+console.log(areaOfaCircle2(1)); 
 
 /******************
 Temperature Converter Fahrenheit to Celsius
@@ -87,14 +105,19 @@ Now store a fahrenheit temperature into a variable.
 
 Convert it to celsius and output "NN°F is NN°C."
 ******************/
+
+// function declaration 
 function fahrenheitToCelsius(fahrenheit){
     
-    let fTemperature  = 9 / 5 - 32; 
-    return fTemperature;
+     return fahrenheit = (9 / 5) - 32; 
+    
 
 }    
 console.log(fahrenheitToCelsius(1));
 
+// function expression 
+const fahrenheitToCelsius2 = fahrenheit2 => (9 / 5) - 32;
+console.log(fahrenheitToCelsius2());
 
 /******************
 Temperature Converter Celsius to Fahrenheit
@@ -105,6 +128,7 @@ Store a celsius temperature into a variable.
 
 Convert it to fahrenheit and output "NN°C is NN°F".
 ******************/
+//function declaration 
 function celsiusToFahrenheit(celsius){
 
     let cTemperature = 9 / 5 + 32
@@ -112,6 +136,12 @@ function celsiusToFahrenheit(celsius){
 
 }
 console.log(celsiusToFahrenheit(1));
+
+//function expression 
+
+const celsiusToFahrenheit2 = celsius2 => (9 / 5) + 32;
+console.log(celsiusToFahrenheit2());
+
 /******************
 Is it the weekend?
 
@@ -130,16 +160,18 @@ If you are having trouble, please note that Javascript has a helpful built-in fu
  function isItTheWeekend(){
      let weekend = ['Saturday', 'Sunday']; 
 
-     if(weekend === 'Saturday' && 'Sunday'){
-         console.log("Yes");
+     if(weekend === 'Saturday' || weekend === 'Sunday'){
+         console.log("Yes, Its a weekend");
      }else {
-         console.log("No");
+         console.log("No,Its  the weekday");
      }
      
      return weekend; 
  }
 
+console.log(isItTheWeekend("Saturday"));
 
+*/ 
 /******************
 Finding the absolute value of a number
 
@@ -150,12 +182,19 @@ The function should return the absolute value of the number
 The absolute value of a negative number is the positive version of that same number,
 and the absolute value of a positive number (or zero) is that number itself.
 ******************/
+//function declaration 
+
 function absolute(){
   Math.abs(10);
   return 10; 
 
 }
 console.log(absolute(1));
+
+//function expression 
+const absolute2 = n => Math.abs(n); 
+console.log(absolute2(10));
+
 /******************
 Create a function that counts the number of characters in your name
 
@@ -167,7 +206,7 @@ return the number of characters in the string
 call function 'countChars'
 ******************/
 
-function countChars(){
+/* function countChars(){
         
        let myName = 15 ; 
 
@@ -175,4 +214,4 @@ function countChars(){
 
 
 }
-console.log(myName.toString()); 
+console.log(myName.toString()); */
