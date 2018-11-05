aa6ea3964e577875394f07d4197a3955da9a2b23
ositaigwe@pursuit.org <j0h@example.com>
2018-11-05 13:18:16 -0500

    add solution for pset6

--- pset6.js
+++ pset6.js
@@ -8,7 +8,11 @@
     @example addTwoNumbers(1,2) // 3
     @example addTwoNumbers(1) // 1
 */
-
+const addTwoNumbers = (a=0,b=0) =>{
+    return a+b;
+}
+console.log(addTwoNumbers(1,2));
+console.log(addTwoNumbers(1));
 
 /*
     @func turnNumberToString
@@ -19,6 +23,17 @@
     @example turnNumberToString(1); // "1"
     @example turnNumberToString("2"); // "2"
 */
+const turnNumberToString = (a) => {
+    if(typeof a === 'number'){
+        return `${a}`;
+    }
+    else if(typeof a === 'string'){
+        return a;
+    }
+    throw new Error('please insert a number');
+}
+console.log(turnNumberToString(1));
+console.log(turnNumberToString("2"));
 
 /*
     @func fullName
@@ -30,7 +45,17 @@
     @example fullName('Taq', 'Karim'); // "Taq Karim"
     @example fullName('Foo'); // "Foo"
 */
-
+const fullName = (firstName, lastName) => {
+    if (typeof firstName === 'string' && typeof lastName === 'undefined') {
+        return firstName;
+    }
+    else if (typeof firstName === 'string' && typeof lastName === 'string'){
+       return firstName.concat(' ', lastName);
+    }
+    throw new Error('please insert a firstName and lastName, as strings')
+}
+console.log(fullName('Taq', 'Karim'));
+console.log(fullName('Foo'));
 /*
     @func fullNameSentence
     @param {string} firstName
@@ -45,7 +70,10 @@
         // expect: "John Smith is awesome"
         fullNameSentence('John', 'Smith', 'is awesome');
 */
-
+const fullNameSentence = (firstName, lastName, restOfSentence) => {
+    return fullName(firstName,lastName).concat(' ', restOfSentence);
+}
+console.log(fullNameSentence('John', 'Smith', 'is awesome'));
 /*
     @func fullNameSentenceWithChecks
     @param {string} firstName
@@ -65,7 +93,17 @@
         // expect: 'Required variables are not set!'
         fullNameSentenceWithChecks('John', 'Smith');
 */
+const isRequired = () => { 
+    throw new Error('param is required'); 
+}
 
+const fullNameSentenceWithChecks = (firstName, lastName = isRequired(), restOfSentence = isRequired()) => { 
+    return fullName(firstName, lastName).concat(' ', restOfSentence );
+}
+
+ 
+
+console.log(fullNameSentence('John', 'Smith', 'is awesome'));
 /*
     @func fToC
     @param {number} f
@@ -77,6 +115,16 @@
     @example fToC(); // 0
     
 */
+const fToC = (number) => {
+    if (number === undefined) {
+        return 0;
+    }
+    let celsius = (number - 32) * 5/9;
+    return celsius;
+}
+console.log(fToC( 32 ));
+console.log(fToC( 212 ));
+console.log(fToC());
 
 /*
     @func fToKelvin
@@ -93,6 +141,16 @@
     @exmple fToC( 212 ); // 373.15
     @example fToC(); // 273.15
 */
+
+const fToKelvin = (number) => {
+let kelvin = number + 273.15;
+return kelvin;
+}
+console.log('---',fToKelvin(fToC( 32 )));
+console.log('---',fToKelvin(fToC( 212 )));
+console.log('---',fToKelvin(fToC()));
+
+
 
 /*
     @func fToKelvinWithChecks
@@ -111,4 +169,13 @@
     @example fToC(); // "ERROR: variable 'f' is not set"
 */
 
+const fToKelvinWithChecks = (number) => {
+    if (typeof number === 'undefined'){
+        throw new Error("ERROR: variable 'f' is not set");
+    }
+    return fToKelvin(fToC(number));
+}
+console.log(fToKelvinWithChecks(32));
+console.log(fToKelvinWithChecks(212));
+console.log(fToKelvinWithChecks());
 
